996005419c18120ed0eeef6e8f20e049a2f3ad74
Abhirup-261004 <abhirupn2004@gmail.com>
2025-11-23 16:24:50 +0530

    Add Routes

---
+++ app.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const path = require("path");
+const User = require("./models/user");
+const userRoutes= require("./routes/user")
+
+
+dotenv.config();
+const app = express();
+
+
+  
+const store = MongoStore.create({
+  mongoUrl: process.env.MONGO_URI,
+  touchAfter: 24 * 60 * 60,
+  crypto: {
+    secret: process.env.SESSION_SECRET   // FIXED
+  }
+});
+store.on('error', function(e){
+  console.log('SESSION STORE ERROR', e);
+});
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log(" MongoDB Connected"))
+  .catch((err) => console.log(" MongoDB Error:", err));
+// EJS Setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(ejslayouts);
+app.set("layout", "layouts/boilerplate.ejs");
+
+// Middlewares
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+// Sessions + Flash
+app.use(session({
+  store:store,
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+
+// Passport Setup
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Use local strategy
+passport.use(
+  new LocalStrategy({ usernameField: "email" }, User.authenticate())
+);
+// Serialize / deserialize user
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Flash + Global Variables
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  res.locals.currentUser = req.user;
+  next();
+});
+
+
+// Routes
+app.use("/", userRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running at http://localhost:${PORT}`));
